2b3ec5b968b153a10f9e954fcd7fc0ba51948790
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-01-18 02:40:18 +0300

    script that lists all bands with Glam rock as their main style

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,20 +1,10 @@
 -- script that lists all bands with Glam rock as their main style
 -- ranked by their longevity
-CREATE TEMPORARY TABLE IF NOT EXISTS tmp_glam_rock_lifespan AS (
-    SELECT
-        band_name,
-        IFNULL((YEAR('2022-01-17') - FORMED), 0) AS lifespan
-    FROM
-        metal_bands
-    WHERE
-        style = 'Glam rock'
-);
-
 -- list Gram rock bands ranked by lifespan
-SELECT
-    band_name,
-    lifespan
-FROM
-    tmp_glam_rock_lifespan
-ORDER BY
-    lifespan DESC;
+
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+    FROM metal_bands
+    WHERE
+        FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY
+        lifespan DESC;
