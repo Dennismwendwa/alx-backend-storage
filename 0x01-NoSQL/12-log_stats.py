0bc6f69b6aae231a36879420b8ffdf8879d9fa65
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-01-23 08:15:50 +0300

    printing Nginx log file stats

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""This script provide stats about Nginx logs"""
+from pymongo import MongoClient
+
+
+def printing_nginx_log_stats(log_collection):
+    """Printing Nginx log stats"""
+    print(f"{log_collection.count_documents({})} logs")
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        log_count = len(list(log_collection.find({"method": method})))
+        print(f"\tmethod {method}: {log_count}")
+    status_count = len(list(
+        log_collection.find({"method": "GET", "path": "/status"})
+    ))
+    print(f"{status_count} status check")
+
+
+def main():
+    """Prints Nginx stats log in Mongo database"""
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    printing_nginx_log_stats(client.logs.nginx)
+
+
+if __name__ == "__main__":
+    main()
